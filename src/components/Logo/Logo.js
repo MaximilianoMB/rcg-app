28e31fdadb4e87c69b4905f9221394ea64791cf6
Maximiliano MB <maques.mb@gmail.com>
2022-06-10 17:48:57 -0300

    app copiado

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Logo.css'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <div>
+                <div className='tilty br2 shadow-2' >👽</div>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
